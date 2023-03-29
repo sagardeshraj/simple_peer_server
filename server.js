ae0a70adbcbb3faecdaac5001748e22c499e249e
sagardeshraj <saagardeshraj14@gmail.com>
2023-03-30 00:04:18 +0530

    cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://remarkable-fudge-821e0a.netlify.app",
 		methods: [ "GET", "POST" ]
 	}
 })
